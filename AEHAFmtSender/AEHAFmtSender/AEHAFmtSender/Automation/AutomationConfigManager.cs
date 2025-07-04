80004500fdcff3d810acd979ef329a36ac214562
Lausiv1024 <75715137+Lausiv1024@users.noreply.github.com>
2025-07-04 10:59:06 +0900

    自動設定の項目の設定

--- AEHAFmtSender/AEHAFmtSender/AEHAFmtSender/Automation/AutomationConfigManager.cs
+++ AEHAFmtSender/AEHAFmtSender/AEHAFmtSender/Automation/AutomationConfigManager.cs
@@ -1,12 +1,42 @@
-﻿namespace AEHAFmtSender.Automation;
+﻿using System.Reflection;
+using System.Text.Json;
+
+namespace AEHAFmtSender.Automation;
 
 public class AutomationConfigManager
 {
-    public bool AircondPwrLink = false;
-    public bool AircondAutoPower = false;
+    readonly string ProgramDirectory = Directory.GetParent(Assembly.GetExecutingAssembly().Location).FullName;
+
+    public AutomationConfig? Config { get; set; }
 
     public AutomationConfigManager()
     {
+        var fPath = Path.Combine(ProgramDirectory, "AutomationConfig.json");
 
+        if (File.Exists(fPath))
+        {
+            using var sr = new StreamReader(fPath);
+            try
+            {
+                Config = JsonSerializer.Deserialize<AutomationConfig>(sr.ReadToEnd());
+            }catch (Exception ex)
+            {
+                Console.WriteLine("Error Loading config {0}", ex.Message);
+            }
+        }
     }
+
+    public void Save()
+    {
+        if (Config == null)
+            return;
+        using var sw = new StreamWriter(Path.Combine(ProgramDirectory, "AutomationConfig.json"));
+        sw.Write(JsonSerializer.Serialize(Config));
+    }
+}
+
+public class AutomationConfig
+{
+    public bool AircondPwrLink = false;
+    public bool AircondAutoPower = false;
 }
